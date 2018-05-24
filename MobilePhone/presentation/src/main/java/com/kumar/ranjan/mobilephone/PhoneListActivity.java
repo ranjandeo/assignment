0e825147c1823ce16738aa19df58649ea21a8e13
ranjandeo <ranjandeo@gmail.com>
2018-05-25 03:50:31 +0700

    Creating project

---
+++ MobilePhone/presentation/src/main/java/com/kumar/ranjan/mobilephone/PhoneListActivity.java
@@ -0,0 +1,13 @@
+package com.kumar.ranjan.mobilephone;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PhoneListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_phone_list);
+    }
+}
