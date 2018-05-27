e99777cb6d9ad66919bdb979cc5a9f8d573b488d
ranjandeo <ranjandeo@gmail.com>
2018-05-28 03:52:41 +0700

    Implementing swipe feature to remove from the favorite lits

---
+++ MobilePhone/presentation/src/main/java/com/kumar/ranjan/mobilephone/screen/helper/ButtonsState.java
@@ -0,0 +1,7 @@
+package com.kumar.ranjan.mobilephone.screen.helper;
+
+enum ButtonsState {
+    GONE,
+    LEFT_VISIBLE,
+    RIGHT_VISIBLE
+}
---
+++ MobilePhone/presentation/src/main/java/com/kumar/ranjan/mobilephone/screen/helper/SwipeControllerActions.java
@@ -0,0 +1,9 @@
+package com.kumar.ranjan.mobilephone.screen.helper;
+
+public abstract class SwipeControllerActions {
+
+    public void onLeftClicked(int position) {}
+
+    public void onRightClicked(int position) {}
+
+}
